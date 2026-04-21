e11b777ec1878bd48e549b463a99d4c4c747c1b4
kgacandole <kevin.candole@gmail.com>
2026-04-21 11:19:34 +0800

    added log and updated sequence

--- scripts/bash/setup_prereq_gcp.sh
+++ scripts/bash/setup_prereq_gcp.sh
@@ -30,56 +30,41 @@ full_svc_account_id_compute="${svc_account_name_compute}@${projectId}.iam.gservi
 #     --attribute-condition="assertion.repository_owner == '$org_name'"
 
 # Add binding to the devops-svc-account for the identity pool to impersonate access (use a diff account to execute below cmd)
-#gcloud iam service-accounts add-iam-policy-binding <devops_svc_account> --member="principalSet://iam.googleapis.com/projects/<gcp_project_num>/locations/global/workloadIdentityPools/github-pool/attribute.repository/<org_name>/<repo_name>" --role="roles/iam.serviceAccountTokenCreator"
+# gcloud iam service-accounts add-iam-policy-binding <devops_svc_account> --member="principalSet://iam.googleapis.com/projects/<gcp_project_num>/locations/global/workloadIdentityPools/github-pool/attribute.repository/<org_name>/<repo_name>" --role="roles/iam.serviceAccountTokenCreator"
 
 # Add binding to the svc-account to allow granting of project-level binding
 # gcloud projects add-iam-policy-binding <project_id> --member="serviceAccount:<devops_svc_account>@<project_id>.iam.gserviceaccount.com" --role="roles/resourcemanager.projectIamAdmin" --condition=None
 ## ----------------------------------------------------------------- ##
 
-# Create Bucket for state file
-echo "Creating TF State Bucket.."
+echo "Create Bucket for state file.."
 gcloud storage buckets describe "gs://${tfstate_bucket_name}" || gcloud storage buckets create "gs://${tfstate_bucket_name}" \
     --location="${region^^}" \
     --uniform-bucket-level-access
 
-# Create service account for Terraform
 echo "Creating Service Account for Terraform"
 gcloud iam service-accounts describe "$full_svc_account_id_tf" || gcloud iam service-accounts create "$svc_account_name_tf" --display-name="Service Account for Terraform"
 
-# Create service account for Compute (Nodes)
 echo "Creating Service Account for Kubernetes Nodes.."
 gcloud iam service-accounts describe "$full_svc_account_id_compute" || gcloud iam service-accounts create "$svc_account_name_compute" --display-name="Service Account for Kube Cluster/Compute resources"
 
-# Add policy to bucket
-gcloud storage buckets add-iam-policy-binding "gs://${tfstate_bucket_name}" \
-    --member="serviceAccount:${full_svc_account_id_tf}" \
-    --role="roles/storage.admin"
+echo "Updating policy on TF State Bucket.."
+gcloud storage buckets add-iam-policy-binding "gs://${tfstate_bucket_name}" --member="serviceAccount:${full_svc_account_id_tf}" --role="roles/storage.admin"
 
-# Allow GitHub OIDC identities to act as svc account        
-gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" \
-    --role="roles/iam.workloadIdentityUser" \
-    --member="principalSet://iam.googleapis.com/projects/${projectNum}/locations/global/workloadIdentityPools/${identity_pool_name}/attribute.repository_owner/${org_name}"
+echo "Adding IAM bindings on TF Svc Account.."
 
-# Allow Terraform to request access tokens (needed by provider)
-gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" \
-    --role="roles/iam.serviceAccountTokenCreator" \
-    --member="principalSet://iam.googleapis.com/projects/${projectNum}/locations/global/workloadIdentityPools/${identity_pool_name}/attribute.repository_owner/${org_name}"
+# Allowing Github OIDC to act as TF Svc Account
+gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" --role="roles/iam.workloadIdentityUser" --member="principalSet://iam.googleapis.com/projects/${projectNum}/locations/global/workloadIdentityPools/${identity_pool_name}/attribute.repository_owner/${org_name}"
 
-# Add bindings - TF Svc Account
-gcloud projects add-iam-policy-binding "$projectId" \
-  --member="serviceAccount:${full_svc_account_id_tf}" \
-  --role="roles/container.admin" \
-  --condition=None
+# Allowing Terraform to request access tokens (needed by provider)
+gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" --role="roles/iam.serviceAccountTokenCreator" --member="principalSet://iam.googleapis.com/projects/${projectNum}/locations/global/workloadIdentityPools/${identity_pool_name}/attribute.repository_owner/${org_name}"
 
-gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" \
-  --member="serviceAccount:${full_svc_account_id_tf}" \
-  --role="roles/iam.serviceAccountUser"
+# Adding service account user role
+gcloud iam service-accounts add-iam-policy-binding "$full_svc_account_id_tf" --member="serviceAccount:${full_svc_account_id_tf}" --role="roles/iam.serviceAccountUser"
 
-gcloud projects add-iam-policy-binding "$projectId" \
-  --member="serviceAccount:${full_svc_account_id_tf}" \
-  --role="roles/compute.viewer"
+echo "Adding Project-level bindings on TF Svc Account.."
+gcloud projects add-iam-policy-binding "$projectId" --member="serviceAccount:${full_svc_account_id_tf}" --role="roles/container.admin" --condition=None
 
-# Add bindings - Nodes Svc Account
-gcloud projects add-iam-policy-binding "$projectId" \
-    --member="serviceAccount:${full_svc_account_id_compute}" \
-    --role="roles/container.defaultNodeServiceAccount"
+gcloud projects add-iam-policy-binding "$projectId" --member="serviceAccount:${full_svc_account_id_tf}" --role="roles/compute.viewer"
+
+echo "Adding project-level bindings on Kube Nodes Svc Account.."
+gcloud projects add-iam-policy-binding "$projectId" --member="serviceAccount:${full_svc_account_id_compute}" --role="roles/container.defaultNodeServiceAccount"
