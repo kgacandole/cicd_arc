9d7d029c829abc984a9076204d64e78849f0c641
kgacandole <kevin.candole@gmail.com>
2026-04-21 17:25:30 +0800

    renamed and added az cmds

---
+++ scripts/bash/setup_prereq_azure.sh
@@ -0,0 +1,15 @@
+
+region="$1"
+
+resourceGroup="arc_tfstate_rg"
+storageAccount="arcstorageacct"
+storageContainer="arc_tfstate_container"
+
+echo "Creating Resource Group.."
+az group create --name "$resourceGroup" --location "$region"
+
+echo "Create Storage Account.."
+az storage account create --name "$storageAccount" --resource-group "$resourceGroup" --location "$region" --sku Standard_LRS --kind StorageV2
+
+echo "Create container on the Storage Account.."
+az storage container create --name "$storageContainer" --account-name "$storageAccount" --auth-mode login
